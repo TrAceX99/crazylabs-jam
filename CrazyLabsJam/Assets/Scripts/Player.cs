cbc051ef41e2c86085864ad2acc00e275a8e7037
Kalmaxis <41956849+Kalmaxis@users.noreply.github.com>
2022-04-22 23:08:54 +0200

    Da proverim dal sam ja

--- CrazyLabsJam/Assets/Scripts/Player.cs
+++ CrazyLabsJam/Assets/Scripts/Player.cs
@@ -5,6 +5,15 @@ public class Player : MonoBehaviour {
     private void Update() {
         if (Input.touchCount > 0) {
             Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    break;
+                case TouchPhase.Moved:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
