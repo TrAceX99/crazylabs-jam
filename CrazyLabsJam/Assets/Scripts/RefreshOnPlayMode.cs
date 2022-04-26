3f6eca7049049ec0f9a5a323cca34a75e8c01c70
TrAceX <djole1271952@gmail.com>
2022-04-26 22:23:11 +0200

    !EXPERIMANTAL! Disabled Unity editor lag/freeze

---
+++ CrazyLabsJam/Assets/Scripts/RefreshOnPlayMode.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+using UnityEngine;
+
+// Refresh asset database on entering play mode. Make sure to disable asset auto-refresh in Preferences.
+ 
+public static class EnterPlayMode
+{
+    [RuntimeInitializeOnLoadMethod]
+    private static void Init()
+    {
+        EditorApplication.playModeStateChanged += OnEnterPlayMode;
+    }
+ 
+    private static void OnEnterPlayMode(PlayModeStateChange obj)
+    {
+        if (obj == PlayModeStateChange.ExitingEditMode) AssetDatabase.Refresh(ImportAssetOptions.Default);
+    }
+}
+ 
