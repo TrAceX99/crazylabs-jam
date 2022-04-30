0f4946fe2121f5e62f7a6a3152efe501ee5f74ad
TrAceX <djole1271952@gmail.com>
2022-04-30 17:43:03 +0200

    Monsters tweaking

--- CrazyLabsJam/Assets/Scripts/CustomizableSpawner.cs
+++ CrazyLabsJam/Assets/Scripts/CustomizableSpawner.cs
@@ -4,10 +4,10 @@ public class CustomizableSpawner : MonoBehaviour {
     [Range(0f, 1f)]
     [SerializeField] float spawnChance;
 
-    private void Awake() {
+    private void Start() {
         if (spawnChance == 1f) return;
 
-        if (Random.Range(0f, 1f) > spawnChance) {
+        if (Random.Range(0f, 1f) >= spawnChance) {
             GetComponent<DragFeature>().Done();
             Destroy(gameObject);
         }
