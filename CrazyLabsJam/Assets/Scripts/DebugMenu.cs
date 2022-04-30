98c53578baefc18acdd15becfed11edc369879dc
TrAceX <djole1271952@gmail.com>
2022-04-30 16:17:42 +0200

    DebugMenu comments

--- CrazyLabsJam/Assets/Scripts/DebugMenu.cs
+++ CrazyLabsJam/Assets/Scripts/DebugMenu.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
 public class DebugMenu : Singleton<DebugMenu> {
-    public float cameraDistance = 0f;
-    public float cameraSpeed = 0f;
-    public float volume = 1f;
-    public float pullGiveMultiplier = 1f;
+    public float cameraDistance = 0f; // -5f to 5f
+    public float cameraSpeed = 1f; // 0.5f to 2f 
+    public float volume = 1f; // 0f to 1f
+    public float pullGiveMultiplier = 1f; // 0.5f to 2f
 
+    // Three buttons, id = 0, 1, 2
     public void SpawnNewMonster(int id) {
         GameManager.Instance.RemoveMonster();
         GameManager.Instance.NewMonster(id);
