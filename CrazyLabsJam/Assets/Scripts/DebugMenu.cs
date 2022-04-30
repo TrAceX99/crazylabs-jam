077220186a156734d44e703d0bd79a88844a729a
TrAceX <djole1271952@gmail.com>
2022-04-30 16:14:51 +0200

    DebugMenu 2

--- CrazyLabsJam/Assets/Scripts/DebugMenu.cs
+++ CrazyLabsJam/Assets/Scripts/DebugMenu.cs
@@ -4,8 +4,7 @@ public class DebugMenu : Singleton<DebugMenu> {
     public float cameraDistance = 0f;
     public float cameraSpeed = 0f;
     public float volume = 1f;
-
-    
+    public float pullGiveMultiplier = 1f;
 
     public void SpawnNewMonster(int id) {
         GameManager.Instance.RemoveMonster();
